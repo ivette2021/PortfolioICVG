20bd4058d444ce8b3918bc425a131e4d24e8fd94
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2023-12-16 00:52:51 -0300

    arreglo de grid y se agrego ux

---
+++ hovernav.js
@@ -0,0 +1,8 @@
+function setActive(element) {
+    // Eliminar la clase 'active' de todos los elementos del menú
+    const menuItems = document.querySelectorAll('nav ul li a');
+    menuItems.forEach(item => item.classList.remove('active'));
+
+    // Agregar la clase 'active' al elemento clicado
+    element.classList.add('active');
+}
