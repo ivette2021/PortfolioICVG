4929ab2228368334e5e95014ad9bc45340ebb595
Ivette C Valdivia Gomez <iconstanzav1@gmail.com>
2024-01-03 02:46:10 -0300

    se separo el header en un arcchivo aparte

--- inputHeader.js
+++ inputHeader.js
@@ -1,17 +1,25 @@
+// Cargar el contenido del archivo y agregarlo al elemento con id "navigation-placeholder"
+var navigationPlaceholder = document.getElementById('navigation-placeholder');
+var xhr = new XMLHttpRequest();
 
-    // Cargar el contenido del archivo y agregarlo al elemento con id "navigation-placeholder"
-    var navigationPlaceholder = document.getElementById('navigation-placeholder');
-    var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        navigationPlaceholder.innerHTML = xhr.responseText;
+        
+        // Crear un elemento link para el archivo CSS
+        var linkElement = document.createElement('link');
+        linkElement.rel = 'stylesheet';
+        linkElement.href = 'header.css'; // Reemplaza con la ruta correcta de tu archivo CSS
 
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            navigationPlaceholder.innerHTML = xhr.responseText;
-            var div = document.createElement('div');
-            div.innerHTML = xhr.responseText;
-                // Insertar el contenido debajo del encabezado
-               
-        }
-    };
+        // Añadir el enlace al head del documento
+        document.head.appendChild(linkElement);
 
-    xhr.open('GET', navigationPlaceholder.getAttribute('data-source'), true);
-    xhr.send();
+        // Insertar el contenido debajo del encabezado
+        var div = document.createElement('div');
+        div.innerHTML = xhr.responseText;
+        // ... tu lógica para insertar el contenido
+    }
+};
+
+xhr.open('GET', navigationPlaceholder.getAttribute('data-source'), true);
+xhr.send();
