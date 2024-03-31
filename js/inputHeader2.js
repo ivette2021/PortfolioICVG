73c3a05491f3159ef009fc1809594813acd20377
ivette <iconstanzav1@gmail.com>
2024-03-31 00:03:24 -0300

    orden en el codigo, se agrego un nuevo apartado de skills, se borraron html y css que no se estaban usando , trabajdno en lo responsivo

---
+++ js/inputHeader2.js
@@ -0,0 +1,25 @@
+// Cargar el contenido del archivo y agregarlo al elemento con id "navigation-placeholder"
+var navigationPlaceholder = document.getElementById('navigation-placeholder');
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        navigationPlaceholder.innerHTML = xhr.responseText;
+        
+        // Crear un elemento link para el archivo CSS
+        var linkElement = document.createElement('link');
+        linkElement.rel = 'stylesheet';
+        linkElement.href = '../css/styleSkills.css'; // Reemplaza con la ruta correcta de tu archivo CSS
+
+        // Añadir el enlace al head del documento
+        document.head.appendChild(linkElement);
+
+        // Insertar el contenido debajo del encabezado
+        var div = document.createElement('div');
+        div.innerHTML = xhr.responseText;
+        // ... tu lógica para insertar el contenido
+    }
+};
+
+xhr.open('GET', navigationPlaceholder.getAttribute('data-source'), true);
+xhr.send();
